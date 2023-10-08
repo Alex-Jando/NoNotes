ac9f5a5f27bc4875cc843475ded66c3f8e898edc
akixuu <75064265+akixuu@users.noreply.github.com>
2023-10-07 18:45:07 -0700

    transcription integration with frontend

--- src/flask_site.py
+++ src/flask_site.py
@@ -1,9 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from transcription_generator import transcription_generator as tg
+from transcription_generator import TRANSCRIPTION_OUTPUT_DIR, get_transcription
+import os
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route('/')
-def home():
-    return(render_template('home.html'))
+@app.route('/mp3tonotes', methods=['POST', 'GET'])
+def mp3tonotes():
+    if request.method == 'POST':
+        try:
+            file = request.files['audio']
+            if file:
+                file.save('../uploads/audio.wav')
+                
+                # create transcription
+                tg()
 
+                # wait until transcription is done
+                while get_transcription() is None:
+                    pass
+                
+                os.remove('../uploads/audio.wav')
+                return render_template('mp3tonotes.html', message='success', transcription=get_transcription())
+            else:
+                return render_template('mp3tonotes.html')
+        except Exception as e:
+            return "An error occurred: " + str(e)
+    else:
+        return render_template('mp3tonotes.html')
+        
 app.run(debug=True, host='localhost', port=80)
