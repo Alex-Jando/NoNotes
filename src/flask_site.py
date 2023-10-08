a09ba8d582652d737491a8c34ceec4a6974ffdc6
Alex-Jando <alexjando2007@outlook.com>
2023-10-07 21:57:23 -0700

    Re updated flask_site.py transcription

--- src/flask_site.py
+++ src/flask_site.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, jsonify
+from transcription_generator import transcription_generator as tg
+from transcription_generator import TRANSCRIPTION_OUTPUT_DIR, get_transcription
+import os
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
+app = Flask(__name__, template_folder='templates')
 
-<<<<<<< Updated upstream
 @app.route('/mp3tonotes', methods=['POST', 'GET'])
 def mp3tonotes():
     return render_template('mp3tonotes.html')
-    
     
 @app.route('/api/mp3tonotes', methods=['POST'])
 def _api_mp3tonotes():
@@ -17,31 +18,6 @@ def _api_mp3tonotes():
             
             # create transcription
             tg()
-=======
-@app.route('/')
-def _():
-    return(render_template('home.html'))
-
-@app.route('/notes')
-<<<<<<< HEAD
-def _notes():
-    return(render_template('home.html'))
-@app.route('/confirmsave')
-def _confirmsave():
-    return(render_template('home.html'))
-=======
-def home():
-    return(render_template('notes.html'))
-
-@app.route('/confirm-save')
-def home():
-    return(render_template('home.html'))
-
-@app.route('/')
-def home():
-    return(render_template('home.html'))
->>>>>>> Stashed changes
-
             # wait until transcription is done
             while get_transcription() is None:
                 pass
@@ -54,16 +30,20 @@ def home():
     except Exception as e:
         return jsonify({'error': str(e)})
 
+@app.route('/')
+def _home():
+    return(render_template('home.html'))
+
 @app.route('/notes')
 def _notes():
+    return(render_template('notes.html'))
+
+@app.route('/confirm-save')
+def _confirm_save():
     return(render_template('home.html'))
-@app.route('/confirmsave')
-def _confirmsave():
-    return(render_template('home.html'))
+
 @app.route('/')
 def _():
-    return redirect('/mp3tonotes')
-    # return(render_template('home.html'))
->>>>>>> 394d844b895d8ce286882875550a4efbf81787df
+    return(render_template('home.html'))
 
 app.run(debug=True, host='localhost', port=80)
