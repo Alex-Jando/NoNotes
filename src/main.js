e4bd93687ae3a3d09ccb041a9511cb3de73875b9
Altrz <75871308+Altrz@users.noreply.github.com>
2023-10-07 18:09:27 -0700

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,20 @@
+//API to handle audio recording
+ 
+var audioRecorder = {
+    /** Start recording the audio
+      * @returns {Promise} - returns a promise that resolves if audio recording successfully started
+      */
+    start: function () {
+    //...
+    },
+    /** Stop the started audio recording
+      * @returns {Promise} - returns a promise that resolves to the audio as a blob file
+      */
+    stop: function () {
+    //...
+    },
+    /** Cancel audio recording*/
+    cancel: function () {
+    //...
+    }
+}
